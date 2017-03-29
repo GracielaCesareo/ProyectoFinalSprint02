b6ae506f208ca3d143c7084d74be97a473ea8f16
GracielaCesareo <graciela.cesareoc@gmail.com>
2017-03-29 11:17:56 -0600

    nueva funcion para agregar lista

--- main.js
+++ main.js
@@ -1,33 +1,18 @@
-
 function agregarLista() {
+  var anterior = document.getElementById("titulo");
+  var nombreLista = document.getElementById("nuevaLista").value;
+  var nombreTitulo = anterior.innerHTML = (nombreLista);
+
   var seccion = document.getElementById("listas");
-  var crearTitulo = document.createElement("H3");
-  var titulo = document.getElementById("nuevaLista").value;
-  var nombreTitulo = document.createTextNode(titulo)
-
-  seccion.appendChild(crearTitulo);
-  crearTitulo.appendChild(nombreTitulo)
-
   var listaNueva = document.createElement("UL");
   seccion.appendChild(listaNueva);
 
 }
 
-function crearBotonElementos() {
-  var seccion = document.getElementById("listas")
-  var boton = document.createElement("BUTTON")
-  var nombreBoton = document.createTextNode("Agregar pendientes");
-  var input = document.createElement("INPUT");
-  seccion.appendChild(input);
-  seccion.appendChild(boton);
-  boton.appendChild(nombreBoton);
-}
-
-/*function agregarElemento() {
-  var lista = document.getElementById("lista");
+function agregarElemento() {
   var elemento = document.createElement("LI");
   var texto = document.createTextNode(document.getElementById("nuevoPendiente").value);
 
   elemento.appendChild(texto);
   lista.appendChild(elemento);
-}*/
+}
